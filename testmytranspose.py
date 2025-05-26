0828557c75f0f4c1b7bc17d37a44bd6cd32b74d0
dohyuk <Dohyuk123@github.com>
2025-05-26 17:21:38 +0900

    commit V2

--- testmytranspose.py
+++ testmytranspose.py
@@ -20,3 +20,19 @@ def mytranspose(x):
 
 # ------------------ 테스트 ------------------
 
+# (3) DataFrame의 경우
+D = np.array([1, 2, 3, 4])
+E = np.array(["red", "white", "red", np.nan])
+F = np.array([True, True, True, False])
+mydata3 = pd.DataFrame({"d": D, "e": E, "f": F})
+transposed_df = mytranspose(mydata3)
+assert isinstance(transposed_df, pd.DataFrame)
+assert transposed_df.shape == (3, 4)  # 열이 행으로, 행이 열로 바뀜
+print("3번 통과")
+
+# (4) PyTorch Tensor의 경우
+np_array = np.array([[1, 2], [3, 4]])
+tensor_pt = torch.tensor(np_array)
+transposed_tensor = mytranspose(tensor_pt)
+assert torch.equal(transposed_tensor, torch.tensor([[1, 3], [2, 4]]))
+print("4번 통과")
